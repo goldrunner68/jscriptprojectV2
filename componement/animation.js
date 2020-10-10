c0ca8c229c3794eb2c1fb7cda7b93fb972092095
goldrunner68 <pilouweb2000@gmail.com>
2020-10-10 15:57:28 +0200

    affichage texte selon scroll de la page web ok mais a optimiser v2

--- componement/animation.js
+++ componement/animation.js
@@ -1,31 +1,33 @@
-
+let bool=null;
 function animation() {
 
     // detection du scanAllItem de la page web
 
-let bool;
-
-let id = setInterval(()=>{
-    let winScroll = window.scrollY;
-    let maxHauteur = window.innerHeight;
 
 
-   if ( (bool !==0 && winScroll>=0 && winScroll<=(maxHauteur/3)) )
-   {console.log("1");
+
+
+    let id2 = setInterval(() => {
+        let winScroll = window.scrollY;
+        let maxHauteur = window.innerHeight;
+
+
+        if ((bool !== 0 && winScroll >= -1 && winScroll <= (maxHauteur / 3))) {
+            console.log(bool);
             scan(0);
 
-}
-    if ( ( bool !==1 && winScroll>=(maxHauteur/3) && winScroll<=(maxHauteur/(2/3))) )
-    {console.log("2");
-        scan(1);
+        }
+        if ((bool !== 1 && winScroll >= (maxHauteur / 3) && winScroll <= (maxHauteur / (2 / 3)))) {
+            console.log(bool);
+            scan(1);
 
-    }
-    if ( (bool !==2 && winScroll>=(maxHauteur/(2/3)) && winScroll>=maxHauteur ))
-    {console.log("3");
-        scan(2);
+        }
+        if ((bool !== 2 && winScroll >= (maxHauteur / (2 / 3)) && winScroll >= maxHauteur)) {
+            console.log(bool);
+            scan(2);
 
-    }
-},0)
+        }
+    }, 0)
 
 
 
@@ -38,6 +40,7 @@ let id = setInterval(()=>{
         let itemGridDroite = document.querySelectorAll(".item-grid-droite").item(val);
         let id = setInterval(()=>{//stage 2
             if (inc===100) {
+
                 clearInterval(id);
 
 
@@ -45,10 +48,10 @@ let id = setInterval(()=>{
                 inc += 10;
                 itemGridGauche.style.left = -(100-inc) + "%";
                 itemGridDroite.style.right = -(100-inc) + "%";
-                return bool= val;
+
             }
         }, 10);
-
+return bool=val;
 
 
 
