b24886b5f53a91460923ce57a4b303c05459fb85
goldrunner68 <pilouweb2000@gmail.com>
2020-10-10 13:28:18 +0200

    affichage texte selon scroll de la page web ok mais a optimiser

--- componement/animation.js
+++ componement/animation.js
@@ -1,51 +1,59 @@
 
-let id;
-let winScroll = window.scrollY;
-let hauteur = window.innerHeight;
-let total;
-
-let id2;
-let un = false;
-let deux = false;
-let trois = false;
 function animation() {
 
     // detection du scanAllItem de la page web
 
-    total = (winScroll * 100) / hauteur;//donne la valeur en pourcentage
-    total = (total / 100);// arrondi le pourcentage en entier
+let bool;
 
-            for (let i=0;i<=2;i++){
-            if (scanAllItem(i) !==true){
-                console.log('true');
-                scanAllItem(i);
+let id = setInterval(()=>{
+    let winScroll = window.scrollY;
+    let maxHauteur = window.innerHeight;
 
-            }
+
+   if ( (bool !==0 && winScroll>=0 && winScroll<=(maxHauteur/3)) )
+   {console.log("1");
+            scan(0);
+
 }
+    if ( ( bool !==1 && winScroll>=(maxHauteur/3) && winScroll<=(maxHauteur/(2/3))) )
+    {console.log("2");
+        scan(1);
+
+    }
+    if ( (bool !==2 && winScroll>=(maxHauteur/(2/3)) && winScroll>=maxHauteur ))
+    {console.log("3");
+        scan(2);
+
+    }
+},0)
 
 
 
 
-    function scanAllItem(val=0) {//stage 1
+    let scan = function scanAllItem(val) {//stage
+
         let inc = 0;
-        console.log("val "+val);
+
         let itemGridGauche = document.querySelectorAll(".item-grid-gauche").item(val);
         let itemGridDroite = document.querySelectorAll(".item-grid-droite").item(val);
-        id = setInterval(()=>{//stage 2
+        let id = setInterval(()=>{//stage 2
             if (inc===100) {
-
                 clearInterval(id);
+
+
             } else {
                 inc += 10;
                 itemGridGauche.style.left = -(100-inc) + "%";
                 itemGridDroite.style.right = -(100-inc) + "%";
-
+                return bool= val;
             }
         }, 10);
-        inc = 0;
-        return true;
+
+
+
 
     }
+
 }
 
 
