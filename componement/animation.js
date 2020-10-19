2a49eecf2318359bd7f6d6b00460935ce64cdc2f
goldrunner68 <pilouweb2000@gmail.com>
2020-10-19 15:42:43 +0200

    reconstruction v0.1b

--- componement/animation.js
+++ componement/animation.js
@@ -1,63 +1,48 @@
 let bool=null;
+let winScroll;
+let maxHauteur;
 function animation() {
-
     // detection du scanAllItem de la page web
 
-
-
-
-
     let id2 = setInterval(() => {
-        let winScroll = window.scrollY;
-        let maxHauteur = window.innerHeight;
-
+        winScroll = window.scrollY;
+        maxHauteur = window.innerHeight;
 
         if ((bool !== 0 && winScroll >= -1 && winScroll <= (maxHauteur / 3))) {
-
             scan(0);
-
         }
         if ((bool !== 1 && winScroll >= (maxHauteur / 3) && winScroll <= (maxHauteur / (2 / 3)))) {
-
             scan(1);
-
         }
         if ((bool !== 2 && winScroll >= (maxHauteur / (2 / 3)) && winScroll >= maxHauteur)) {
-
             scan(2);
-
         }
     }, 0)
 
 
 
 
-    let scan = function scanAllItem(val) {//stage
-
+    let scan = function scanAllItem(val=0) {//stage
         let inc = 0;
-
+        let item = document.querySelectorAll(".gauche");
         let itemGridGauche = document.querySelectorAll(".gauche").item(val);
         let itemGridDroite = document.querySelectorAll(".droite").item(val);
+
         let id = setInterval(()=>{//stage 2
-            if (inc===100) {
-
+            if (inc>=100) {
                 clearInterval(id);
-
-
             } else {
+
                 inc +=5;
-                console.log(inc);
+                //console.log("inc "+inc+" val "+val);
                 itemGridGauche.style.left = -(100-inc) + "%";
                 itemGridDroite.style.right = -(100-inc) + "%";
 
             }
         }, 10);
+
 return bool=val;
-
-
-
     }
-
 }
 
 
--- componement/defilement.js
+++ componement/defilement.js
@@ -1,31 +1,25 @@
 
-const data = '{"tableaux":['
-    +
-    '{ "texte":"Résultat d un projet de groupe, l\'objectif était de réaliser selon ' +
-    'un cahier des charges le site web d\'un club, sur le projet j\'avais deux fonctions chef de projet et integrateur." , "fichier":"hbck.jpg" },'
-    +
-    '{ "texte":"Réalisation du site internet du restaurant BHV Mulhouse, ' +
-    'la partie admin contient l\'ajout, supression, organisation et activation ' +
-    '/désactivation des articles " , "fichier":"bhv-samsung.jpg"} ' +
-    ']}';
-
-
-
-
-
-
-
 let defilement = () => {
     let image = 0  ;
 
-const obj = JSON.parse(data);/*convertion du string en objet*/
-const affichageDesProjets = document.querySelector("#defilement");
+    const data = '{"tableaux":['
+        +
+        '{ "texte":"Résultat d un projet de groupe, l\'objectif était de réaliser selon ' +
+        'un cahier des charges le site web d\'un club, sur le projet j\'avais deux fonctions chef de projet et integrateur." , "fichier":"hbck.jpg" },'
+        +
+        '{ "texte":"Réalisation du site internet du restaurant BHV Mulhouse, ' +
+        'la partie admin contient l\'ajout, supression, organisation et activation ' +
+        '/désactivation des articles " , "fichier":"bhv-samsung.jpg"} ' +
+        ']}';
+
+let obj = JSON.parse(data);/*convertion du string en objet*/
+let affichageDesProjets = document.querySelector("#defilement");
 affichageDesProjets.setAttribute('src', 'asset/images/travaux/'+ obj.tableaux[image].fichier);
     document.querySelector('#texteDefilement').innerHTML = obj.tableaux[image].texte;
 /*********************************************************************************************************/
 /* defilement des travaux */
 setInterval(()=> {
-    if (image <= 2) {
+    if (image <= 1) {
         affichageDesProjets.setAttribute('src', 'asset/images/travaux/'+ obj.tableaux[image].fichier);
         document.querySelector('#texteDefilement').innerHTML = obj.tableaux[image].texte;
         image++;
@@ -33,6 +27,6 @@ setInterval(()=> {
         image = 0;
     }
    
-}, 8000);
+}, 4000);
 }
 export {defilement};
